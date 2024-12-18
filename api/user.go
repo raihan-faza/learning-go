d387c3c9fef070eb12484b9c29bcd998b84ea721
lahh <raihanfazasabiqr@gmail.com>
2024-12-18 21:11:31 +0700

    separating api into smaller modules

---
+++ api/user.go
@@ -0,0 +1,15 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+func createUser(c *gin.Context) {
+	return
+}
+
+func updateUser(c *gin.Context) {
+	return
+}
+
+func deleteUser(c *gin.Context) {
+	return
+}
