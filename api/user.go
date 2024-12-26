dc5a280348c953c45ae5685205e3caf8128f6271
lahh <raihanfazasabiqr@gmail.com>
2024-12-26 17:38:57 +0700

    updating user api

--- api/user.go
+++ api/user.go
@@ -1,15 +1,101 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/raihan-faza/learning-go/initializers"
+	"github.com/raihan-faza/learning-go/models"
+)
 
 func createUser(c *gin.Context) {
+	var user models.User
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		c.JSON(
+			400, gin.H{
+				"message": err,
+			},
+		)
+		return
+	}
+	action := initializers.DB.Create(&user)
+	if action.Error != nil {
+		c.JSON(
+			400, gin.H{
+				"message": action.Error,
+			},
+		)
+		return
+	}
+	c.JSON(
+		200, gin.H{
+			"message": "user created",
+		},
+	)
 	return
 }
 
-func updateUser(c *gin.Context) {
+func updateUser(c *gin.Context, userId int) {
+	var user models.User
+	query := initializers.DB.First(&user, userId)
+	if query.Error != nil {
+		c.JSON(
+			400, gin.H{
+				"message": query.Error,
+			},
+		)
+		return
+
+	}
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		c.JSON(
+			400, gin.H{
+				"message": err,
+			},
+		)
+		return
+	}
+	action := initializers.DB.Save(&user)
+	if action.Error != nil {
+		c.JSON(
+			400, gin.H{
+				"message": action.Error,
+			},
+		)
+		return
+	}
+	c.JSON(
+		200, gin.H{
+			"message": "user updated",
+		},
+	)
 	return
 }
 
-func deleteUser(c *gin.Context) {
+func deleteUser(c *gin.Context, userId int) {
+	var user models.User
+	query := initializers.DB.First(&user, userId)
+	if query.Error != nil {
+		c.JSON(
+			400, gin.H{
+				"message": query.Error,
+			},
+		)
+		return
+	}
+	action := initializers.DB.Delete(&user)
+	if action.Error != nil {
+		c.JSON(
+			400, gin.H{
+				"message": action.Error,
+			},
+		)
+		return
+	}
+	c.JSON(
+		200, gin.H{
+			"message": "user deleted",
+		},
+	)
 	return
 }
